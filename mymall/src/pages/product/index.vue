41f1e131800af3af1c2ad6fdc18edc4fcd63d498
admin <1968293570@qq.com>
2019-03-05 21:48:51 +0800

    第二次

--- mymall/src/pages/product/index.vue
+++ mymall/src/pages/product/index.vue
@@ -4,7 +4,7 @@
             <i class="iconfont icon-back" slot="left" @click="goBack()"></i>
             <div slot="center" class="navbar-center">商品详情</div>
             <i class="iconfont icon-msg" slot="right"></i>
-           
+           <!-- 萨沙的男孩 -->
         </navbar>
         <div class="product">
             <!-- 商品图片开始 -->
